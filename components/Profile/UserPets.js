19e58ced3955b9f958ef8e7fc06216033e7a5a10
kwkry <vincekurt1@gmail.com>
2024-05-07 01:46:57 +0800

    Fixed mismatched parameter in user pets component

--- components/Profile/UserPets.js
+++ components/Profile/UserPets.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function UserPets({ userPetInfo }) {
-  console.log("Pet Info :", userPetInfo);
-  const hasPetInfo = userPetInfo && Array.isArray(userPetInfo) && userPetInfo.length > 0;
+export default function UserPets({ petInfo }) {
+  console.log("Pet Info :", petInfo);
+  const hasPetInfo = petInfo && Array.isArray(petInfo) && petInfo.length > 0;
 
   return (
     <View style={styles.container}>
       {hasPetInfo ? (
         <>
-          <Text>Breed: {userPetInfo[0].breed}</Text>
-          <Text>Contact: {userPetInfo[0].contact}</Text>
-          <Text>Location: {userPetInfo[0].location}</Text>
-          <Text>Pet Name: {userPetInfo[0].petName}</Text>
-          <Text>Pet Type: {userPetInfo[0].petType}</Text>
+          <Text>Breed: {petInfo[0].breed}</Text>
+          <Text>Contact: {petInfo[0].contact}</Text>
+          <Text>Location: {petInfo[0].location}</Text>
+          <Text>Pet Name: {petInfo[0].petName}</Text>
+          <Text>Pet Type: {petInfo[0].petType}</Text>
         </>
       ) : (
         <Text>Loading...</Text>
